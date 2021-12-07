906f8e96bb7d25c07327200475d007985739c3a0
udj009 <judiclei41@gmail.com>
2021-12-07 17:55:49 -0300

    repositories added

---
+++ src/main/java/com/udj/bookStore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.udj.bookStore.repository;
+
+import com.udj.bookStore.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
---
+++ src/main/java/com/udj/bookStore/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.udj.bookStore.repository;
+
+import com.udj.bookStore.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
